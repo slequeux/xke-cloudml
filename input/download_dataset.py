5e77953c34ed31d9bcf87964aea8c79ca70fdb33
Sylvain Lequeux <slequeux@xebia.fr>
2018-04-09 14:55:12 +0200

    fx download dataset

--- input/download_dataset.py
+++ input/download_dataset.py
@@ -6,7 +6,7 @@ PROJECT = 'aerobic-coast-161423'
 BUCKET = 'sleq-ml-engine'
 FILE = 'data.zip'
 PATH = 'data/%s' % FILE
-DEST = '%s/../' % os.path.realpath(__file__)
+DEST = os.path.dirname(os.path.realpath(__file__))
 
 
 if os.path.exists('%s/%s' % (DEST, FILE)):
@@ -16,7 +16,7 @@ else:
     bucket = storage_client.get_bucket(BUCKET)
     blob = bucket.blob(PATH)
     print('Downloading %s/%s' % (BUCKET, PATH))
-    blob.download_to_filename(DEST)
+    blob.download_to_filename('%s/%s' % (DEST, FILE))
     print('Downloaded')
 
 
